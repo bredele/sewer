dd659b93375f778c57af6fe60cb42c7248f09443
bredele <olivier.wietrich@gmail.com>
2014-01-17 16:29:21 -0700

    add test repo

---
+++ index.js
@@ -0,0 +1,20 @@
+var attach = window.addEventListener ? 'addEventListener' : 'attachEvent',
+    detach = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
+    prefix = listen !== 'addEventListener' ? 'on' : '';
+
+
+/**
+ * Expose 'Events'
+ */
+
+module.exports = Events;
+
+
+/**
+ * Events constructor.
+ * @api public
+ */
+
+function Events() {
+  //do something
+}
