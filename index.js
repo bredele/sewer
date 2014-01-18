d72767e41011c8f48a6d615ada8032e67a774cc9
bredele <olivier.wietrich@gmail.com>
2014-01-18 14:22:38 -0700

    add remove listeners

--- index.js
+++ index.js
@@ -3,45 +3,64 @@
  * Polyfill
  */
 
-var attach = window.addEventListener ? 'addEventListener' : 'attachEvent',
-    detach = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
-    prefix = attach !== 'addEventListener' ? 'on' : '';
+ var attach = window.addEventListener ? 'addEventListener' : 'attachEvent',
+     detach = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
+     prefix = attach !== 'addEventListener' ? 'on' : '';
 
 
+/**
+ * Matches query selection.
+ * 
+ * @param  {HTMLElement} el 
+ * @param  {HTMLElement} target  
+ * @param  {String} selector 
+ * @return {Boolean}  true if the element would be selected by the 
+ * specified selector string
+ */
 function matches(el, target, selector) {
 	//refactor with maple (childnodes indexof)
 	return [].slice.call(el.querySelectorAll(selector)).indexOf(target) > -1;
 }
 
+
 /**
- * [bind description]
- * @return {[type]} [description]
+ * Attach Event Listener.
+ * 
+ * @param  {HTMLElement}   el
+ * @param  {String}   str
+ * @param  {Function} fn 
+ * @param  {Boolean}   capture
+ * @return {Array} handler to detach event      
  */
 
  exports.listen = function(el, str, fn, capture) {
- 	var filter = str.split('>');
- 	var phrase = filter[0].split(' '),
- 			topic = phrase.shift(),
- 			selector = phrase.join(' ');
+ 	var filter = str.split('>'),
+ 	    phrase = filter[0].split(' '),
+ 	    topic = phrase.shift(),
+ 	    selector = phrase.join(' ');
 
- 	if(selector) {
- 		el[attach](prefix + topic, function(ev) {
- 			var target = ev.target || ev.srcElement,
- 			 		code = filter[1] && filter[1].replace(/ /g,'');
- 			if(matches(el, target, selector)) {
- 				if(!code || ev.keyCode.toString() === code) fn(target, ev); //is it the order right?
- 			}
- 		}, capture || false);
- 	} else {
- 		el[attach](prefix + topic, function(ev){
+ 	var cb = function(ev) {
+ 		if(!selector || matches(el, ev.target || ev.srcElement, selector)) {
  			var target = ev.target || ev.srcElement,
  			    code = filter[1] && filter[1].replace(/ /g,'');
 
  			if(!code || ev.keyCode.toString() === code) fn(target, ev);
- 		}, capture || false);
- 	}
+ 		}
+ 	};
+
+  el[attach](prefix + topic, cb, capture || false);
+ 	return [topic, cb, capture];
  };
 
+
+/**
+ * Detach event listener.
+ * 
+ * @param  {HTMLElement}   el
+ * @param  {String}   str
+ * @param  {Function} fn
+ * @param  {Boolean}   capture   
+ */
 
  exports.detach = function(el, str, fn, capture) {
  	el[detach](prefix + str, fn, capture || false);
